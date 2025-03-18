aaf8b55d7232b9a5daf9dbe20bb4ab14f360708b
jwon0523 <jwon010523@gmail.com>
2025-03-18 22:39:45 +0900

    init: 프로젝트 생성 & gitignore 생성

---
+++ iOS_Practice/1st_Practice/1st_Practice/_st_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  _st_PracticeApp.swift
+//  1st_Practice
+//
+//  Created by jaewon Lee on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct _st_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
