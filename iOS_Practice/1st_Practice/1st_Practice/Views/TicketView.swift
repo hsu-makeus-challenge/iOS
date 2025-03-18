451943029463fd01b784b9e863849f39f88a38a2
jwon0523 <jwon010523@gmail.com>
2025-03-18 23:20:12 +0900

    feat: mainTitleGroup 뷰 생성

--- iOS_Practice/1st_Practice/1st_Practice/Views/TicketView.swift
+++ iOS_Practice/1st_Practice/1st_Practice/Views/TicketView.swift
@@ -9,22 +9,27 @@ import SwiftUI
 
 struct TicketView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, World!")
+        ZStack {
+            Image(.ticketBg)
+            mainTitleGroup
         }
         .padding()
-        .onAppear {
-            UIFont.familyNames.sorted().forEach { familyName in
-                print("*** \(familyName) ***")
-                UIFont.fontNames(forFamilyName: familyName).forEach { fontName in
-                    print("\(fontName)")
-                }
-                print("-------------------------")
+    }
+    
+    private var mainTitleGroup: some View {
+        VStack {
+            Group { // Group이 뭔지 찾아보기
+                Text("마이펫의 이중생활2")
+                    .font(.PretendardBold30)
+                    .shadow(color: .black.opacity(0.25), radius: 2, x: 0, y: 4)
+                Text("본인 + 동반 1인")
+                    .font(.PretendardLight16)
+                Text("30,100원")
+                    .font(.PretendardBold24)
             }
+            .foregroundStyle(Color.white)
         }
+        .frame(height: 84)
     }
 }
 
