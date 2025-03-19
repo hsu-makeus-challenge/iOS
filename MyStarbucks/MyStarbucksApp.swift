dee9a3a0a4dcdd0b60401e8bb3483bb0d9568e97
codenameVien <ejej2003@naver.com>
2025-03-20 00:26:56 +0900

    Initial Commit

---
+++ MyStarbucks/MyStarbucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyStarbucksApp.swift
+//  MyStarbucks
+//
+//  Created by 이은정 on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct MyStarbucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyStarbucksTests/MyStarbucksTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyStarbucksTests.swift
+//  MyStarbucksTests
+//
+//  Created by 이은정 on 3/20/25.
+//
+
+import Testing
+@testable import MyStarbucks
+
+struct MyStarbucksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
