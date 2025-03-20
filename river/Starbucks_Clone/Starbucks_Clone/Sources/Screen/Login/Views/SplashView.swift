4b49caee2008d2b77819fe9757664e4a942e9e74
jwon0523 <jwon010523@gmail.com>
2025-03-20 14:25:26 +0900

    feat: SplashView 완료

--- river/Starbucks_Clone/Starbucks_Clone/Sources/Screen/Login/Views/SplashView.swift
+++ river/Starbucks_Clone/Starbucks_Clone/Sources/Screen/Login/Views/SplashView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct SplashView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .center) {
+            Color(.green01)
+            Image(.starbuckLogo)
+        }
+        .ignoresSafeArea()
     }
 }
 
