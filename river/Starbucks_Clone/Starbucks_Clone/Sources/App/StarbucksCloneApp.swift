d1e876e30919c2839545a0b6f0d0caefa0a8f3f7
jwon0523 <jwon010523@gmail.com>
2025-03-20 17:59:54 +0900

    feat: 스플래쉬 화면과 로그인 화면 연결

--- river/Starbucks_Clone/Starbucks_Clone/Sources/App/StarbucksCloneApp.swift
+++ river/Starbucks_Clone/Starbucks_Clone/Sources/App/StarbucksCloneApp.swift
@@ -2,9 +2,22 @@ import SwiftUI
 
 @main
 struct StarbucksCloneApp: App {
+    @State private var showMainView: Bool = false
+    
     var body: some Scene {
         WindowGroup {
-            LoginView()
+            if showMainView {
+                LoginView()
+            } else {
+                SplashView()
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                            withAnimation {
+                                showMainView = true
+                            }
+                        }
+                    }
+            }
         }
     }
 }
