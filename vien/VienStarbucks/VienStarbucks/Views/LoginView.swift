c6d543998878f16a67ec1323923a08db6610272f
codenameVien <ejej2003@naver.com>
2025-03-23 14:03:54 +0900

    style: 주석제거

--- vien/VienStarbucks/VienStarbucks/Views/LoginView.swift
+++ vien/VienStarbucks/VienStarbucks/Views/LoginView.swift
@@ -1,28 +1,5 @@
 import SwiftUI
 
-// 아 이렇게 묶으면 전체 패딩을 한번에 줄수 잇구나.. 또 배운다
-// 그리고 이렇게 나눌시에 다루기도 편하고 그런듯
-//struct LoginView: View {
-//    var body: some View {
-//        VStack {
-//            WelcomeView()
-//            
-//            Spacer().frame(height: 104)
-//            
-//            IdPwdView()
-//
-//            Spacer().frame(height:47)
-//
-//            LoginButtonView()
-//            
-//            Spacer().frame(height:104)
-//            
-//            SignUpView()
-//            
-//        }
-//        .padding(.horizontal, 19)
-//    }
-//}
 struct LoginView: View {
     var body: some View {
         GeometryReader { geometry in
@@ -50,30 +27,6 @@ struct LoginView: View {
     }
 }
     
-//
-//struct WelcomeView: View {
-//    var body: some View {
-//        VStack(alignment: .leading) {
-//            Image(.starbucksLogo)
-//                .resizable()
-//                .frame(width: 97, height: 95)
-//            
-//            Spacer().frame(height: 28)
-//            
-//            Text("안녕하세요.\n스타벅스입니다.")
-//                .foregroundColor(.black)
-//                .font(.mainTextBold24)
-//                .fixedSize(horizontal: false, vertical: true) // 자동 크기 조정
-//            
-//            Spacer().frame(height: 19)
-//            
-//            Text("회원 서비스 이용을 위해 로그인 해주세요")
-//                .foregroundColor(Color("gray01"))
-//                .font(.mainTextMedium16)
-//        }
-//        .frame(maxWidth: .infinity, alignment: .leading) // 왼쪽 정렬 위해
-//    }
-//}
 struct WelcomeView: View {
     var geometry: GeometryProxy
     
@@ -101,26 +54,6 @@ struct WelcomeView: View {
 }
 
 
-
-//
-//struct IdPwdView: View {
-//    var body: some View {
-//        VStack(alignment: .leading) {
-//            Text("아이디")
-//                .font(.mainTextRegular13)
-//            Divider()
-//                .background(Color("gray00"))
-//                
-//            Spacer().frame(height:47)
-//                
-//            Text("비밀번호")
-//                .font(.mainTextRegular13)
-//            
-//            Divider()
-//                .background(Color("gray00"))
-//        }
-//    }
-//}
 struct IdPwdView: View {
     var geometry: GeometryProxy
     
@@ -160,62 +93,6 @@ struct LoginButtonView: View {
 }
 
 
-
-//struct SignUpView: View {
-//    var body: some View {
-//        //Email SignUp
-//        Text("이메일로 회원가입하기")
-//            .underline()
-//            .font(.mainTextRegular13)
-//            .foregroundColor(Color("gray03"))
-//        
-//        Spacer().frame(height:19)
-//     
-//        //이미지불러오기보단 그냥 직접 버튼 만드는게 좋을 것 같다 하셧으니 만들어보기
-//
-//        Button {
-//            print("카카오 로그인")
-//        } label: {
-//            HStack {
-//                Image(.kakaoLogo)
-//                Spacer()
-//                Text("카카오 로그인")
-//                    .font(.mainTextMedium16)
-//                    .foregroundStyle(.black)
-//                Spacer()
-//            }
-//            .padding(.horizontal, 14)
-//        }
-//        .frame(height: 45)
-//        .background(Color.kakaobg)
-//        .clipShape(RoundedRectangle(cornerRadius: 5))
-//        .padding(.horizontal, 48)
-//        
-//        
-//        Spacer().frame(height:19)
-//    
-//
-//        Button {
-//            print("Apple Login")
-//        } label: {
-//            HStack {
-//                Image(.appleLogo)
-//                Spacer()
-//                Text("애플 로그인")
-//                    .font(.mainTextMedium16)
-//                    .foregroundStyle(.white)
-//                Spacer()
-//            }
-//            .padding(.horizontal, 14)
-//        }
-//        .frame(height: 45)
-//        .background(Color.black)
-//        .clipShape(RoundedRectangle(cornerRadius: 5))
-//        .padding(.horizontal, 48) // 아 버튼 가로길이를 고정할게 아니라 인피니티로 "양옆" 패딩 주면 크기 기종 바껴도 ㄱㅊ겠구나
-//        
-//      
-//    }
-//}
 struct SignUpView: View {
     var geometry: GeometryProxy
     
@@ -270,9 +147,6 @@ struct SignUpView: View {
     }
 }
 
-
-
-//스플래시 뷰.....
 
 #Preview {
     LoginView()
