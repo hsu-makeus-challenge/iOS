22b5e659f59536549da7515e6c51b4d068a80e6b
jwon0523 <jwon010523@gmail.com>
2025-03-25 22:58:10 +0900

    init: 2주차 실습 프로젝트 생성

---
+++ river/iOS_Practice/Weak2/Weak2/Weak2App.swift
@@ -0,0 +1,17 @@
+//
+//  Weak2App.swift
+//  Weak2
+//
+//  Created by jaewon Lee on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Weak2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
