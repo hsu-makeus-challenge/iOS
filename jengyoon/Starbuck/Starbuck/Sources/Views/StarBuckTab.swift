56bd8f24042aefdf0ac531c773cd40a7da26ad2e
SongCodeMaster <lssyl_@naver.com>
2025-03-27 17:06:26 +0900

    feat: StarBuck TabBar 구현

---
+++ jengyoon/Starbuck/Starbuck/Sources/Views/StarBuckTab.swift
@@ -0,0 +1,111 @@
+//
+//  ContentView.swift
+//  Starbuck
+//
+//  Created by 송승윤 on 3/27/25.
+//
+
+import SwiftUI
+
+/// TabView 선언, Button 선언 방식과 유사한것같음
+struct StarBuckTab: View {
+    var body: some View {
+        TabView {
+            
+            Tab {
+                HomeView()
+            } label: {
+                Label {
+                    Text("Home")
+                }
+                icon: {
+                    Image("home")
+                }
+            } //: label
+            
+            Tab {
+                PayView()
+            } label: {
+                Label {
+                    Text("Pay")
+                }
+                icon: {
+                    Image("pay")
+                }
+            } //: label
+            
+            Tab {
+                OrderView()
+            } label: {
+                Label {
+                    Text("Order")
+                }
+                icon: {
+                    Image("order")
+                }
+            } //: label
+            
+            Tab {
+                ShopView()
+            } label: {
+                Label {
+                    Text("Shop")
+                }
+                icon: {
+                    Image("shop")
+                }
+            } //: label
+            
+            Tab {
+                OtherView()
+            } label: {
+                Label {
+                    Text("Other")
+                }
+                icon: {
+                    Image("other")
+                }
+            } //: label
+            
+        } //: TabView
+        .tint(Color.primaryGreen)
+    }
+}
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home")
+    }
+}
+
+
+struct PayView: View {
+    var body: some View {
+        Text("Pay")
+    }
+}
+
+
+struct OrderView: View {
+    var body: some View {
+        Text("Order")
+    }
+}
+
+
+struct ShopView: View {
+    var body: some View {
+        Text("Shop")
+    }
+}
+
+
+struct OtherView: View {
+    var body: some View {
+        Text("Other")
+    }
+}
+
+#Preview {
+    StarBuckTab()
+}
