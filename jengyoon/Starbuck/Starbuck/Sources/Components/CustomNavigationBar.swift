a68bd433b44f4fbfc4b65b91634386656e133072
SongCodeMaster <lssyl_@naver.com>
2025-04-03 02:16:03 +0900

    feat: 버튼 활성화 비활성화 로직 구현 및 네비게이션 타이틀 safeArea기준 배치

--- jengyoon/Starbuck/Starbuck/Sources/Components/CustomNavigationBar.swift
+++ jengyoon/Starbuck/Starbuck/Sources/Components/CustomNavigationBar.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 /// 네비게이션바 커스텀
 /// SwiftUI에서는 현재 네비게이션 바 커스텀하려면 뒤로가기버튼 숨김처리하고 뷰를 직접 만드는 방법을 주로 사용한다.
+/// - Parameters
+/// - title: navigationTitle 부분, String받아와 title지정 해줌
+/// - onback: 뒤로가기 콜백함수 SignUpView에서는 dismiss를 받아온다.
 struct CustomNavigationBar: View {
     let title: String
     let onBack: () -> Void
@@ -26,7 +29,8 @@ struct CustomNavigationBar: View {
                 .font(.PretendardMedium16)
             
             Spacer()
+            
+            Spacer().frame(width: 20) // 공간줘서 비율 맞추기
         }
-        .padding(.leading, 19)
     }
 }
--- jengyoon/Starbuck/Starbuck/Sources/Views/Login/SignupView.swift
+++ jengyoon/Starbuck/Starbuck/Sources/Views/Login/SignupView.swift
@@ -9,72 +9,82 @@ import SwiftUI
 
 struct SignupView: View {
     // MARK: - Properties
-    @State private var viewModel = SignupViewModel()
+    /// 해당 뷰에서 객체를 생성하여 관리하므로 @StateObject로 선언
+    @StateObject private var viewModel = SignupViewModel()
     /// dismiss 사용하여 하위뷰 pop
     @Environment(\.dismiss) private var dismiss
-    
+    /// 입력필드 실시간으로 바인딩하기 위해 State로 선언하고 버튼 활성화
+    @State private var inputNickname = ""
+    @State private var inputEmail = ""
+    @State private var inputPassword = ""
     var body: some View {
-        
-        VStack (alignment: .leading) {
-            
-            Spacer().frame(height: 210)
-            
-            TextField("닉네임", text: $viewModel.nickname)
-            
-            Divider()
-            
-            Spacer().frame(height: 49)
-            
-            TextField("이메일", text: $viewModel.email)
-            
-            Divider()
-            
-            Spacer().frame(height: 49)
-            
-            SecureField("비밀번호", text: $viewModel.password)
-            
-            Divider()
-            
-            Spacer().frame(height: 428)
-            
-            createButton
-            
-            Spacer().frame(height: 100)
+        ZStack {
+            Color.white.ignoresSafeArea()
+            VStack (alignment: .leading) {
+                CustomNavigationBar(title: "가입하기") {
+                    dismiss()
+                }
+                
+                Spacer()
+                
+                TextField("닉네임", text: $inputNickname)
+                
+                Divider()
+                
+                Spacer().frame(height: 49)
+                
+                TextField("이메일", text: $inputEmail)
+                
+                Divider()
+                
+                Spacer().frame(height: 49)
+                
+                SecureField("비밀번호", text: $inputPassword)
+                
+                Divider()
+                
+                Spacer()
+                
+                createButton
+                
+                
+            }
+            .safeAreaPadding(.horizontal,20)
+            .safeAreaPadding(.vertical, 20)
+            .navigationBarBackButtonHidden(true)
         }
-        .padding(.horizontal, 20)
-        .padding(.bottom, 20)
-        .navigationTitle("스타벅스 회원가입")
-        .navigationBarBackButtonHidden(true)
-        
     }
     
     // MARK: - Components
     private var createButton: some View {
         Button (action: {
-            // AppStorage에서 저장된 값 사용
-            dismiss()
+            // 유효성 통과시에 AppStorage에 저장되도록
+            // 모든 항목이 유효할 때만 실행되도록
+            if buttonValid {
+                viewModel.nickname = inputNickname
+                viewModel.email = inputEmail
+                viewModel.password = inputPassword
+                dismiss()
+            }
         }) {
             Text("생성하기")
                 .font(.PretendardRegular18)
                 .foregroundStyle(Color.white)
                 .frame(maxWidth: 400, minHeight: 50)
-                .background(Color.primaryGreen)
+                .background(buttonValid ? Color.primaryGreen : Color.gray)
                 .clipShape(RoundedRectangle(cornerRadius: 15))
         }
+        .disabled(
+            !buttonValid
+        )
     }
     
-    /// 뒤로가기 버튼 커스텀
-    private var backButton: some View {
-        Button(action: {
-            dismiss()
-        }, label: {
-            HStack {
-                Image(systemName: "chevron.left")
-                    .aspectRatio(contentMode: .fit)
-                foregroundStyle(Color.primaryGreen)
-                Text("뒤로 가기")
-            }
-        })
+    /// 버튼 활성화 비활성화 상태 체크
+    /// - 세 항목이 모두 1글자 이상 채워져있어야 버튼이 활성화 되도록 유효성 검사
+    private var buttonValid: Bool {
+        !inputNickname.isEmpty &&
+        !inputEmail.isEmpty &&
+        !inputPassword.isEmpty
     }
 }
 
