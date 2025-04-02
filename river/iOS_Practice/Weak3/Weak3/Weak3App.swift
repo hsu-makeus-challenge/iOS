42dd6bb487892f2fab8b63e71b8c940e00eaf7c9
jwon0523 <jwon010523@gmail.com>
2025-04-03 00:42:57 +0900

    3주차 워크북 실습 완료

---
+++ river/iOS_Practice/Weak3/Weak3/Weak3App.swift
@@ -0,0 +1,17 @@
+//
+//  Weak3App.swift
+//  Weak3
+//
+//  Created by jaewon Lee on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Weak3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
