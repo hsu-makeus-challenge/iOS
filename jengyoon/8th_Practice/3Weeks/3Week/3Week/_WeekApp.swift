b979b73a402b9dc9d057a809da82c7f8fa15578e
Mr.Song <lssyl_@naver.com>
2025-04-04 22:27:22 +0900

    3주차 실습 파일 업로드 (#16)
    * feat: 3주차 navigation 연결 및 상태관리 실습 완료
    
    * fix: contentView 삭제 후 WindowGroup 변경

---
+++ jengyoon/8th_Practice/3Weeks/3Week/3Week/_WeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  _WeekApp.swift
+//  3Week
+//
+//  Created by 송승윤 on 3/31/25.
+//
+
+import SwiftUI
+
+@main
+struct _WeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RainbowView()
+        }
+    }
+}
