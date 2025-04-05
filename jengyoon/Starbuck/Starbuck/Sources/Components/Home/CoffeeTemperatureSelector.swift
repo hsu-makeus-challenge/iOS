b5716e2a7a0bc6bae3579443e1fb19bf146979d4
SongCodeMaster <lssyl_@naver.com>
2025-04-05 15:42:23 +0900

    style:Color 확장자 선언

---
+++ jengyoon/Starbuck/Starbuck/Sources/Components/Home/CoffeeTemperatureSelector.swift
@@ -0,0 +1,46 @@
+//
+//  CoffeeTemperatureSelector.swift
+//  Starbuck
+//
+//  Created by 송승윤 on 4/4/25.
+//
+
+import SwiftUI
+
+/// 커피 상세정보 뷰 Iced/Hot 토글 버튼 Selector
+struct CoffeeTemperatureSelector: View {
+    let type: CoffeeTemperatureType
+    
+    /// 바인딩으로 선언한 이유
+    /// 이 View 내부에서 옵션을 선택해도, 외부 View(CoffeeDetailView)에서도 상태를 알고 있어야 하기 때문에 선언하였음.
+    @Binding var selectedOption: CoffeeOption?
+    
+    var body: some View {
+        HStack {
+            // type.option는 현재 커피의 온도 타입에 따라 나타날 버튼을 결정한다. ([.iced, .hot])
+            ForEach(type.options) { option in
+                Button(action: {
+                    withAnimation {
+                        selectedOption = option
+                    }
+                }) {
+                    Text(option.rawValue)
+                        .font(.PretendardRegular18)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(
+                            selectedOption == option ? Color.white01 : Color.gray.opacity(0.2)
+                        )
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+            }
+        } //: ForEach
+    }
+    
+    /// 선택된 옵션에 따라 텍스트 색상을 바꿔주는 함수 ( ICED : blue , Hot: red)
+    private func getTextColor(for option: CoffeeOption) -> Color {
+        guard selectedOption == option else {
+            return 
+        }
+    }
+}
