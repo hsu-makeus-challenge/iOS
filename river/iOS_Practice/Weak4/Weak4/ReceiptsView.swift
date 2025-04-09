1e95c55e4eb28356e134de999365a4a5b3e4e8ce
Jaewon Lee <50106190+jwon0523@users.noreply.github.com>
2025-04-09 15:42:12 +0900

    Create Weak4 실습(OCR 실습) (#22)
    * feat: OCR을 사용하여 영수증 텍스트 추출 완료
    
    * 텍스트 정규화 완료

---
+++ river/iOS_Practice/Weak4/Weak4/ReceiptsView.swift
@@ -0,0 +1,80 @@
+//
+//  ReceiptsView.swift
+//  Weak4
+//
+//  Created by jaewon Lee on 4/9/25.
+//
+
+import SwiftUI
+import Observation
+
+struct ReceiptsView: View {
+    
+    @Bindable var viewModel: ReceiptsViewModel = .init()
+    
+    var body: some View {
+        VStack(spacing: 42) {
+            HStack {
+                ForEach(ReceiptSegment.allCases, id: \.id) { segment in
+                    topSegment(segment: segment)
+                }
+            }
+            
+            tabView
+        }
+        .frame(maxWidth: 337, maxHeight: .infinity)
+    }
+    
+    @ViewBuilder
+    func topSegment(segment: ReceiptSegment) -> some View {
+        VStack(spacing: 0) {
+            Text(segment.title)
+                .foregroundStyle(Color.black)
+                .font(.system(size: 40))
+                .onTapGesture {
+                    viewModel.selectedSegment = segment
+                }
+            if viewModel.selectedSegment == segment {
+                Rectangle()
+                    .fill(Color.black)
+                    .frame(width: 116, height: 2)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        
+        if segment == .first {
+            Spacer()
+        }
+    }
+    
+    private var tabView: some View {
+        TabView(selection: $viewModel.selectedSegment, content: {
+            ForEach(ReceiptSegment.allCases, id: \.id) { segment in
+                VStack(spacing: 10) {
+                    Image(segment.imageName)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 490)
+                    
+                    if viewModel.selectedSegment == segment {
+                        if let receipt = viewModel.currentReceipt {
+                            ReceiptInfoView(receipt: receipt)
+                        } else {
+                            ProgressView("OCR 처리 중...")
+                        }
+                    }
+                }
+                .tag(segment)
+                /* 세그먼트의 탭 화면이 등장할 때 OCR 추출이 돼야 하기 때문에 task로 작성합니다. */
+                .task {
+                    viewModel.startOCR(segment)
+                }
+            }
+        })
+        .tabViewStyle(.page(indexDisplayMode: .never))
+    }
+}
+
+#Preview {
+    ReceiptsView()
+}
