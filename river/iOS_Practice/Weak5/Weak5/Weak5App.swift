b975207fb4dc18cbe322fbf4a1e4439258573f7a
Jaewon Lee <50106190+jwon0523@users.noreply.github.com>
2025-05-01 14:53:41 +0900

    Create Weak5 실습(JSON파싱) (#27)
    * chore: gitignore 파일에 OSRM 관련 폴더/파일 추가
    
    * feat:JSON 파싱 실습 완료

---
+++ river/iOS_Practice/Weak5/Weak5/Weak5App.swift
@@ -0,0 +1,17 @@
+//
+//  Weak5App.swift
+//  Weak5
+//
+//  Created by jaewon Lee on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Weak5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
