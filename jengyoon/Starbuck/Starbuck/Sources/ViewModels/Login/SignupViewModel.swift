67dbcf7e9e67dec02e2dc44b882e09f2fadf5e68
SongCodeMaster <lssyl_@naver.com>
2025-05-08 13:46:42 +0900

    refector: 기존 회원가입 정보 저장 AppStorage -> KeyChain방식으로 변경

--- jengyoon/Starbuck/Starbuck/Sources/ViewModels/Login/SignupViewModel.swift
+++ jengyoon/Starbuck/Starbuck/Sources/ViewModels/Login/SignupViewModel.swift
@@ -5,31 +5,47 @@
 //  Created by 송승윤 on 3/27/25.
 
 /// 회원가입 화면에서 사용하는 ViewModel
-/// 사용자 입력값을 AppStorage(UserDefaults)와 연동하여 저장
+/// 사용자 입력값을 Keychain에 저장
 import Foundation
 import SwiftUI
 
 class SignupViewModel: ObservableObject {
-    @AppStorage("userEmail") private var userEmail: String = ""
-    @AppStorage("userPassword") private var userPassword: String = ""
-    @AppStorage("userNickname") private var userNickname: String = ""
     
+    // 사용자 입력값
     @Published var email = ""
     @Published var password = ""
     @Published var nickname = ""
+    
+    // 회원가입 완료 여부
     @Published var isSignupComplete = false
     
+    /// 입력 필드 검증 로직
     var isFormValid: Bool {
         !email.isEmpty && !password.isEmpty && !nickname.isEmpty
     }
     
+    /// 회원가입 처리 함수
+    /// 입력값을 Keychain에 저장한다.
     func signup() {
-        // AppStorage에 사용자 정보 저장
-        userEmail = email
-        userPassword = password
-        userNickname = nickname
+        KeychainWrapper.save(email, for: .email)
+        KeychainWrapper.save(password, for: .password)
+        KeychainWrapper.save(nickname, for: .nickname)
         
         // 회원가입 완료 처리
         isSignupComplete = true
     }
-} 
+    
+    /// Keycahin에서 기존 사용자 데이터를 불러와 필드에 반영하기
+    func loadSavedUserData() {
+        email = KeychainWrapper.load(for: .email) ?? ""
+        password = KeychainWrapper.load(for: .password) ?? ""
+        nickname = KeychainWrapper.load(for: .nickname) ?? ""
+    }
+    
+    /// Keychain에 저장된 사용자 정보 삭제
+    func clearUserData() {
+        KeychainWrapper.delete(for: .email)
+        KeychainWrapper.delete(for: .password)
+        KeychainWrapper.delete(for: .nickname)
+    }
+}
