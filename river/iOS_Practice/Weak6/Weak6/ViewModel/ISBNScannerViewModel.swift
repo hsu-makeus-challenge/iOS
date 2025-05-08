e24321ef0edac192d8ca3c2b3ffdacb3ed8f5d1c
jwon0523 <jwon010523@gmail.com>
2025-05-08 18:22:11 +0900

    feat: KakaoAPIService & ISBNScannerViewModel done

--- river/iOS_Practice/Weak6/Weak6/ViewModel/ISBNScannerViewModel.swift
+++ river/iOS_Practice/Weak6/Weak6/ViewModel/ISBNScannerViewModel.swift
@@ -6,8 +6,38 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftUI
 
 @Observable
 class ISBNScannerViewModel {
+    
     var bookModel: BookModel.Documents?
+    var errorMessage: String?
+    
+    var isShowSaveView: Bool = false
+    
+    func searchBook(isbn: String) async {
+        
+        self.errorMessage = nil
+        
+        do {
+            let result = try await KakaoAPIService.shared.searchBook(query: isbn)
+            self.bookModel = result.documents.first
+            self.isShowSaveView = true
+        } catch {
+            self.errorMessage = error.localizedDescription
+            print("전체 에러 정보:", error)
+        }
+    }
+    
+    
+    public func purchaseBook() {
+        if let urlString = self.bookModel?.url,
+           let url = URL(string: urlString) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            print("구매 링크 연결 x")
+        }
+    }
 }
