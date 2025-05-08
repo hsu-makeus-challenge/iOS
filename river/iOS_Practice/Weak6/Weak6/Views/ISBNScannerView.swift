19864e14815a03892f7822d8443e1ce4562b1591
jwon0523 <jwon010523@gmail.com>
2025-05-08 18:44:35 +0900

    feat: ISBNScanncerView done

--- river/iOS_Practice/Weak6/Weak6/Views/ISBNScannerView.swift
+++ river/iOS_Practice/Weak6/Weak6/Views/ISBNScannerView.swift
@@ -7,12 +7,61 @@
 
 import SwiftUI
 
-struct ISBNScannerView: View {
+struct ISBNScanncerView: View {
+    
+    let title: String = "바코드를 스캔해주세요"
+    let subTitle: String = "ISBN 바코드 스캔을 통해 \n책의 정보를 빠르고 정확하게 가져올 수 있어요!"
+    
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        backgroundView()
+    }
+    
+    @ViewBuilder
+    private func backgroundView() -> some View {
+        Image(.scanGuide)
+            .resizable()
+            .overlay(content: {
+                guidlineView()
+            })
+            .ignoresSafeArea(.all)
+    }
+    
+    @ViewBuilder
+    private func guidlineView() -> some View {
+        VStack {
+            VStack(spacing: 10) {
+                Text(title)
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+                Text(subTitle)
+                    .lineLimit(2)
+                    .lineSpacing(2.5)
+                    .multilineTextAlignment(.center)
+            }
+            .foregroundStyle(Color.white)
+            
+            Spacer().frame(height: 453)
+            
+            Button(action: {
+                dismiss()
+            }, label: {
+                Text("나가기")
+                    .font(.body)
+                    .foregroundStyle(Color.black)
+                    .padding(30)
+                    .background {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.white)
+                    }
+            })
+        }
     }
 }
 
 #Preview {
-    ISBNScannerView()
+    ISBNScanncerView()
 }
+
