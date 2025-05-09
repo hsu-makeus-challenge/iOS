a372f1f875e0490dfcd97a186354bf4a34de5d85
Jaewon Lee <50106190+jwon0523@users.noreply.github.com>
2025-05-09 16:58:06 +0900

    Create Week6 Practice6 (#33)
    * chore: 실습에 필요한 폴더 및 파일 생성
    
    * feat: KakaoInterceptor 로직 생성
    
    * feat: KakaoAPIService & ISBNScannerViewModel done
    
    * feat: ISBNScanncerView done
    
    * feat: BarcodeScannerView done
    
    * chore: 주차별 폴더명 변경
    
    * feat: SuccessISBNView done
    
    * chore: project.pbxproj 파일 수정 완료
    
    * chore: scanGuide 이미지 변경

---
+++ river/iOS_Practice/Week6/Week6/Weak6App.swift
@@ -0,0 +1,17 @@
+//
+//  Weak6App.swift
+//  Weak6
+//
+//  Created by jaewon Lee on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Week6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
