9c84b5fd8808c3d0f37556309b8af6dbaa7c81f0
jwon0523 <jwon010523@gmail.com>
2025-05-11 17:21:12 +0900

    feat: xcconfig 파일 생성 후 Config enum 생성

--- river/Starbucks_Clone/Project.swift
+++ river/Starbucks_Clone/Project.swift
@@ -16,6 +16,8 @@ let project = Project(
                     ],
                     "NSLocationWhenInUseUsageDescription": "앱이 실행 중일 때 위치 정보를 사용합니다.",
                     "NSLocationAlwaysUsageDescription": "앱이 백그라운드에서도 위치 정보를 사용합니다."
+                    "KAKAO_API_KEY": "$(KAKAO_API_KEY)"
+                    "KAKAO_LOGIN_URL": "$(KAKAO_LOGIN_URL)"
                 ]
             ),
             sources: ["Starbucks_Clone/Sources/**"],
---
+++ river/Starbucks_Clone/Starbucks_Clone/Sources/Core/Utilities/Config.swift
@@ -0,0 +1,31 @@
+//
+//  Config.swift
+//  Starbucks_Clone
+//
+//  Created by jaewon Lee on 5/11/25.
+//
+
+import Foundation
+
+enum Config {
+    private static let infoDictionary: [String: Any] = {
+        guard let dict = Bundle.main.infoDictionary else {
+            fatalError("Plist 없음")
+        }
+        return dict
+    }()
+    
+    static let kakaoAPIKey: String = {
+        guard let apiKey = Config.infoDictionary["KAKAO_API_KEY"] as? String else {
+            fatalError()
+        }
+        return apiKey
+    }()
+    
+    static let kakaoLoginURL: String = {
+        guard let loginURL = Config.infoDictionary["KAKAO_LOGIN_URL"] as? String else {
+            fatalError()
+        }
+        return loginURL
+    }()
+}
