477dabf4b7713c7ed46db9cb025a40bc1c097b1b
codenameVien <ejej2003@naver.com>
2025-06-22 22:56:26 +0900

    feat : stickyheader추가

---
+++ vien/VienStarbucks/VienStarbucks/Shop/OffsetModifier.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+
+struct OffsetModifier: ViewModifier {
+    @Binding var offset: CGFloat
+    var returnromStart: Bool = true
+    @State private var startValue: CGFloat = 0
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay(
+                GeometryReader { proxy in
+                    Color.clear
+                        .preference(key: OffsetKey.self, value: proxy.frame(in: .named("SCROLL")).minY)
+                        .onPreferenceChange(OffsetKey.self) { value in
+                            if startValue == 0 {
+                                startValue = value
+                            }
+                            offset = value - (returnromStart ? startValue : 0)
+                        }
+                }
+            )
+    }
+}
+
+struct OffsetKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
+}
